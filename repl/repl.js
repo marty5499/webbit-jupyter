5ce9e0834f62be401790ddeda1a01397f6765488
Marty <marty@kingkit101.com>
2022-01-07 18:34:40 +0800

    rename webai to webbit

--- repl/repl.js
+++ repl/repl.js
@@ -40,21 +40,6 @@ cfg.put('cmd', deployCmd)
   return code;
 };
 
-const snapshotCode = `
-from webai import *
-from time import sleep
-repl = UART.repl_uart()
-img = webai.snapshot()
-webai.show(img=img)
-jpg = img.compress(80)
-img = None
-jpg = jpg.to_bytes()
-repl.write("JPGSize:")
-repl.write(str(len(jpg)))
-repl.write('\\r\\n')
-sleep(0.01)
-repl.write(jpg)
-`
 
 class DataTransformer {
   constructor() {
